aecfa88d6db1e289d5263f58496f3e2eff3e7062
yongbinnnn <dbdydqls927@gmail.com>
2025-10-01 16:25:52 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+}
