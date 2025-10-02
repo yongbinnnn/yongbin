a23b9e88fde2041a1d7e47c38611ba1822397b54
yongbinnnn <dbdydqls927@gmail.com>
2025-10-02 17:11:29 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int M, F;
+    while(1)
+    {
+        scanf("%d %d", &M, &F);
+        if(M == 0 && F == 0)
+        {
+            break;
+        }
+        printf("%d\n", M+F);
+    }
+    
+    return 0;
+}
