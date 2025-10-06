a8584d07924557d5cbe9410ffd0409b42f6b8e52
yongbinnnn <dbdydqls927@gmail.com>
2025-10-06 15:30:14 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char emo[21];
+    scanf("%s", emo);
+    
+    printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:", emo);
+    
+    return 0;
+}
