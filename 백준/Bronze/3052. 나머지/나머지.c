b231b2cb11b2989afd411032d5b70d0e1ef62376
yongbinnnn <dbdydqls927@gmail.com>
2025-10-08 19:24:52 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.c
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
-int main(void) 
-{
-    int num[10];
-    int count = 10;
-    
-    for(int i = 0; i < 10; i++)
-    {
-        scanf("%d", &num[i]);
-        num[i] %= 42;
-    }
-    
-    for(int j = 0; j < 10; j++)
-    {
-        for(int k = j+1; k < 11; k++)
-        {
-            if(num[j] == num[k])
-            {
-                count--;
-                break;
-            }
-        }
-    }
-    
-    printf("%d", count);
-    
-    return 0;
+int arr[42];
+
+int main(){
+	int x;
+	int ans = 0;
+	
+	for(int i=0;i<10;i++){
+		scanf("%d",&x);
+		arr[x%42] = 1;
+	}
+	
+	for(int i=0;i<42;i++)
+		ans += arr[i];
+	
+	printf("%d",ans);
+	
+	return 0;
 }
