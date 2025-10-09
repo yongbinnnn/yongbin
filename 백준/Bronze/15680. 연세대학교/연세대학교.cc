59c93a234ee7ec48df1a128b86e489df22d1476b
yongbinnnn <dbdydqls927@gmail.com>
2025-10-09 20:56:42 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    
+    if(N == 1)
+    {
+        printf("Leading the Way to the Future");
+    }
+    else if(N == 0)
+    {
+        printf("YONSEI");
+    }
+    
+    return 0;
+}
