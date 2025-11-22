42825325a5bfce1548201eeebcd6a759e7920e1d
yongbinnnn <dbdydqls927@gmail.com>
2025-11-22 13:52:31 +0900

    [Bronze II] Title: 저항, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.cc
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char color[10];
+    long num, sum = 0;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        scanf("%s", color);
+        
+        if(strcmp(color, "black") == 0) num = 0;
+        else if(strcmp(color, "brown") == 0) num = 1;
+        else if(strcmp(color, "red") == 0) num = 2;
+        else if(strcmp(color, "orange") == 0) num = 3;
+        else if(strcmp(color, "yellow") == 0) num = 4;
+        else if(strcmp(color, "green") == 0) num = 5;
+        else if(strcmp(color, "blue") == 0) num = 6;
+        else if(strcmp(color, "violet") == 0) num = 7;
+        else if(strcmp(color, "grey") == 0) num = 8;
+        else if(strcmp(color, "white") == 0) num = 9;
+        
+        if(i == 0) sum += num*10;
+        else if(i == 1) sum += num;
+        else
+        {
+            for(int i = 0; i < num; i++)
+            {
+                sum *= 10;
+            }
+        }
+    }
+    
+    printf("%ld", sum);
+    
+    return 0;
+}
