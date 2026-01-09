a0771b3334af4205bf0cb061686f682a4e5c218f
yongbinnnn <dbdydqls927@gmail.com>
2026-01-09 15:12:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int k = i; k < n; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
